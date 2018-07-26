1ebd056632ca9181c06e7bc27615843c86afe58a
read -r -p "Enter your email? " email <cbarbusio@gmail.com>
2018-07-26 18:40:52 -0400

    Completed server side validation

--- m3-java-capstone/src/main/java/com/techelevator/npgeek/Model/Objects/Survey.java
+++ m3-java-capstone/src/main/java/com/techelevator/npgeek/Model/Objects/Survey.java
@@ -1,10 +1,23 @@
 package com.techelevator.npgeek.Model.Objects;
 
-public class Survey {
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Survey {
+	
+	@Size(min = 3, max = 5, message = "Please select a park")
 	String parkCode;
+	
+	@NotBlank(message = "Please enter a valid email address")
+	@Email(message="Please enter a valid email address")
 	String email;
+	
+	@Size(min=2, max =2, message = "Please select a state")
 	String state;
+	
+	@NotBlank(message = "Please select an activity level")
 	String activityLevel;
 	
 	public String getParkCode() {
